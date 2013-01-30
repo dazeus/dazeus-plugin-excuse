4d1b26c473d80ff2a56ac067cbca0ef01add605c
Ruben Nijveld <ruben@gewooniets.nl>
2013-01-30 13:58:35 +0100

    Added command line interface, updated to lates dazeus-nodejs version

--- index.js
+++ index.js
@@ -2,97 +2,64 @@ require('js-methods');
 var dazeus = require('dazeus');
 var fs = require('fs');
 
+// some constants, change at your will
 var DATA = "data/excuses.txt";
+var EXCUSE = "excuse";
+var LEARN = 'learn';
+var FORGET = 'forget';
 
-var HELP = "To learn a new excuse use }excuse learn [excuse]. To forget an excuse, use }excuse forget [excuse]. To print a random excuse, use }excuse";
+// and some help messages
+var HELP = "To learn a new excuse use }" + EXCUSE +
+    " " + LEARN + " [excuse]. To forget an excuse, use }" + EXCUSE +
+    " " + FORGET + " [excuse]. To print a random excuse, use }" + EXCUSE + "";
 var KNOWN = "I already know that excuse";
 var NEWEXCUSE = "Now I can handle any situation with an excuse!";
 var UNKNOWN = "That's no excuse!";
 var FORGOTTEN = "You're right, that's a terrible excuse.";
 
-var client = dazeus.connect({path: '/tmp/dazeus.sock'}, function () {
-    client.onCommand('excuse', function (network, user, channel, command, args, what, rest) {
+// lets parse command line args
+var argv = dazeus.optimist().argv;
+dazeus.help(argv);
+var options = dazeus.optionsFromArgv(argv);
+
+// create the client
+var client = dazeus.connect(options, function () {
+    client.onCommand(EXCUSE, function (network, user, channel, command, args, what) {
         if (typeof what !== 'undefined') {
-            if (what === 'learn' && typeof rest !== 'undefined') {
-                if (append(args.trim().substr(5).trim(), DATA)) {
-                    client.reply(network, channel, user, NEWEXCUSE);
-                } else {
-                    client.reply(network, channel, user, KNOWN);
+            var commandExecuted = false;
+
+            // learn command
+            dazeus.isCommand(LEARN, args, function (toLearn) {
+                if (toLearn.trim().length > 0) {
+                    commandExecuted = true;
+                    if (dazeus.appendTo(DATA, toLearn)) {
+                        client.reply(network, channel, user, NEWEXCUSE);
+                    } else {
+                        client.reply(network, channel, user, KNOWN);
+                    }
                 }
-            } else if (what === 'forget' && typeof rest !== 'undefined') {
-                if (remove(args.trim().substr(6).trim(), DATA)) {
-                    client.reply(network, channel, user, FORGOTTEN);
-                } else {
-                    client.reply(network, channel, user, UNKNOWN);
+            });
+
+            // forget command
+            dazeus.isCommand(FORGET, args, function (toForget) {
+                if (toForget.trim().length > 0) {
+                    commandExecuted = true;
+                    if (dazeus.removeFrom(DATA, toForget)) {
+                        client.reply(network, channel, user, FORGOTTEN);
+                    } else {
+                        client.reply(network, channel, user, UNKNOWN);
+                    }
                 }
-            } else {
+            });
+
+            // show some help
+            if (!commandExecuted) {
                 client.reply(network, channel, user, HELP);
             }
+
+        // generate a random excuse
         } else {
-            var randomExcuse = randomFrom(DATA);
-            client.reply(network, channel, user, randomExcuse, false);
+            client.reply(network, channel, user, dazeus.randomFrom(DATA), false);
         }
     });
 });
-
-var randomFrom = function (file) {
-    var array = readFile(file);
-    return array[Math.floor(Math.random() * array.length)];
-};
-
-var readFile = function (file) {
-    var fs = require('fs');
-    return fs.readFileSync(file).toString().split("\n").filter(function (element) {
-        return element.trim().length > 0;
-    });
-};
-
-var writeFile = function (data, file) {
-    var stream = fs.createWriteStream(file, {flags: 'w'});
-    for (var i in data) {
-        if (data.hasOwnProperty(i)) {
-            if (typeof data[i] === 'string') {
-                stream.write(data[i] + "\n");
-            }
-        }
-    }
-    stream.end();
-};
-
-var append = function (word, file) {
-    var array = readFile(file);
-    if (exists(word, file)) {
-        return false;
-    } else {
-        array.push(word);
-        writeFile(array, file);
-        return true;
-    }
-};
-
-var remove = function (word, file) {
-    var array = readFile(file);
-    if (!exists(word, array)) {
-        return false;
-    } else {
-        array = array.filter(function (elem) {
-            return elem.trim() !== word.trim();
-        });
-        writeFile(array, file);
-        return true;
-    }
-};
-
-var exists = function (word, file) {
-    if (typeof file === 'string') {
-        file = readFile(file);
-    }
-    for (var i in file) {
-        if (file.hasOwnProperty(i)) {
-            if (file[i].trim() === word.trim()) {
-                return true;
-            }
-        }
-    }
-    return false;
-};
